12169be5af0c3bb6a3a7c711d04dfbf0d4b5fe77
Ryoma-h777 <chocolate.rh19@ezweb.ne.jp>
2017-06-11 03:32:05 +0900

    SharkCtrl Add OnTrrigerEnter & Exit

--- Assets/Script/SharkControll.cs
+++ Assets/Script/SharkControll.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class SharkControll : MonoBehaviour {
+    public BoatManager _boatMgr;
 
+    const string FISH_L = "FISH_L";
+    const string FISH_M = "FISH_M";
+    const string FISH_S = "FISH_S";
+    const string DUST = "DUST";
+    const string MASH = "MASH";
+    const string SHIP = "Boat";
 
-	public enum SharkStats{
+    public enum SharkStats{
 		Sleeping,
 		Hungry,
 		Eating_ship,
@@ -27,7 +34,9 @@ public class SharkControll : MonoBehaviour {
 	void Start () {
 		_hungryNum = Random.value * 0.8f + 0.2f;
 
-	}
+        _boatMgr = FindObjectOfType<BoatManager>();
+
+    }
 
     public Slider gauge;
 
@@ -74,12 +83,7 @@ public class SharkControll : MonoBehaviour {
 
     void OnCollisionEnter(Collision otherCol)
     {
-        const string FISH_L = "FISH_L";
-        const string FISH_M = "FISH_M";
-        const string FISH_S = "FISH_S";
-        const string DUST   = "DUST";
-        const string MASH   = "MASH";
-        const string SHIP   = "SHIP";
+
 
         const float RECOVERY_FISH_L = 0.30f;
         const float RECOVERY_FISH_M = 0.2f;
@@ -118,16 +122,8 @@ public class SharkControll : MonoBehaviour {
 
     void OnCollisionExit(Collision otherCol)
     {
-        const string FISH_L = "FISH_L";
-        const string FISH_M = "FISH_M";
-        const string FISH_S = "FISH_S";
-        const string DUST = "DUST";
-        const string MASH = "MASH";
-        const string SHIP = "SHIP";
 
-        const float RECOVERY_FISH_L = 0.30f;
-        const float RECOVERY_FISH_M = 0.2f;
-        const float RECOVERY_FISH_S = 0.15f;
+
         switch (otherCol.gameObject.tag)
         {
             case FISH_L:
@@ -164,7 +160,7 @@ public class SharkControll : MonoBehaviour {
         {
             foreach (GameObject gobj in EatList)
             {
-                //
+                _boatMgr.BoatDie(gobj.name);
             }
         }
     }
@@ -172,6 +168,75 @@ public class SharkControll : MonoBehaviour {
 
     void OnTriggerEnter(Collider other) {
 		Debug.Log(other.name);
-	}
+        const float RECOVERY_FISH_L = 0.30f;
+        const float RECOVERY_FISH_M = 0.2f;
+        const float RECOVERY_FISH_S = 0.15f;
+        switch (other.tag)
+        {
+            case FISH_L:
+                _hungryNum += RECOVERY_FISH_L;
+                break;
+            case FISH_M:
+                _hungryNum += RECOVERY_FISH_M;
+
+                break;
+            case FISH_S:
+                _hungryNum += RECOVERY_FISH_S;
+
+                break;
+
+            case DUST:
+                break;
+
+            case MASH:
+                break;
+
+            case SHIP:
+
+                if (!EatList.Contains(other.gameObject)) {
+                    EatList.Add(other.gameObject);
+                }
+                if (_sharkStats == SharkStats.Hungry)
+                {
+                    //start_eat
+                }
+                break;
+
+        }
+    }
+
+    void OnTriggerExit(Collider otherCol)
+    {
+
+
+        switch (otherCol.gameObject.tag)
+        {
+            case FISH_L:
+                break;
+            case FISH_M:
+
+                break;
+            case FISH_S:
+
+                break;
+
+            case DUST:
+                break;
+
+            case MASH:
+                break;
+
+            case SHIP:
+                if (_sharkStats == SharkStats.Hungry)
+                {
+                    if (EatList.Contains(otherCol.gameObject))
+                    {
+                        EatList.Remove(otherCol.gameObject);
+                    }
+                }
+                break;
+
+        }
+    }
 
 }
