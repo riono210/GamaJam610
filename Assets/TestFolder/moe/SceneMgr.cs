0d241db17c883e4188a0cca9a4b9cb1e7e96390b
DESKTOP-S1KJLVG\moe <goodluck.moe@gmail.com>
2017-06-10 23:22:11 +0900

    Scene,main,game,result

---
+++ Assets/TestFolder/moe/SceneMgr.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMgr : MonoBehaviour {
+
+    public string titleSceneName;
+    public string gameMainSceneName;
+    public string resultSceneName;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+           // Debug.Log(SceneManager.GetActiveScene().name);
+            if (SceneManager.GetActiveScene().name == titleSceneName)
+            {
+                SceneManager.LoadScene(gameMainSceneName);
+            }
+
+            if (SceneManager.GetActiveScene().name == gameMainSceneName)
+            {
+                SceneManager.LoadScene(resultSceneName);
+            }
+
+            if (SceneManager.GetActiveScene().name == resultSceneName)
+            {
+                SceneManager.LoadScene(titleSceneName);
+            }
+        }
+
+    }
+}
