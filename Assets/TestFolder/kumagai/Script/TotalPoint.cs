a85bbe1071ed673716164583ffa60ad3ae0d829c
c17011 <c17011@std.it-college.ac.jp>
2017-06-11 16:04:07 +0900

    リザルト画面、スコア

---
+++ Assets/TestFolder/kumagai/Script/TotalPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TotalPoint : MonoBehaviour {
+	public Text _text;
+	// Use this for initialization
+	void Start () {
+		_text = GetComponent<Text> ();
+		_text.text = (GameMgr.Instance._score.treasurePoint*(GameMgr.Instance._score.aliveBoat * 0.1f)).ToString();
+	// Use this for initialization
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
