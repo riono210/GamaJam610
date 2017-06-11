5af74cbb7120ae56900f8030851d66301db666ce
c17016 <c17016@std.it-college.ac.jp>
2017-06-11 15:59:45 +0900

    パーティクルのプレハブとAutoDestroyEffect.cs

---
+++ Assets/Scripts/AutoDestroyEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroyEffect : MonoBehaviour
+{
+    ParticleSystem particle;
+
+    void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        //Delete GameObject when particle playback is over.
+        if (particle.isPlaying == false) Destroy(gameObject);
+    }
+}
