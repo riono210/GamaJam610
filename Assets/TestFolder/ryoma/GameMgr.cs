34ef02666a5eca67a649f130656514b2a439a7f8
c17011 <c17011@std.it-college.ac.jp>
2017-06-11 10:50:11 +0900

    merge error

---
+++ Assets/TestFolder/ryoma/GameMgr.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class Score
+{
+    public int treasurePoint;
+    public int aliveBoat;
+}
+
+[DisallowMultipleComponent]
+public class GameMgr : SingletonMonoBehaviour<GameMgr> {
+
+    public Score _score;
+
+    BoatManager _boatMgr;
+
+    void Awake()
+	{
+		//このクラスのinstanceが2つ存在する場合、新しく作られたほうをDestroyする
+		if (Instance != null && Instance != this)
+		{
+			Debug.Log("Destroy SingletonInstance");
+			Destroy(this.gameObject);
+			return;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public void GameEnd()
+    {
+        _boatMgr = FindObjectOfType<BoatManager>();
+
+        //_score.treasurePoint = _boatMgr;
+        //_score.aliveBoat = _boatMgr.;
+
+        SceneManager.LoadScene("Result");
+    }
+}
